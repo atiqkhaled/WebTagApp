132cbbb807f3c78556ae5ed346e92cc71b457347
System Administrator <root@atiq-khaleds-MacBook-Pro.local>
2017-03-06 14:19:45 +0600

    First Commit

---
+++ WebContent/test.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib prefix="superman" uri="supertaglib"%>
+<html>
+  <head>
+    <title>A SuperMan tag</title>
+  </head>
+  <body>
+   <!-- In superman loop tag count attribute value specified how many times hello world generated on html -->
+    <superman:loop count="3">
+      Hello world!
+    </superman:loop>
+  </body>
+</html>
---
+++ src/com/main/SuperManOptionalTag.java
@@ -0,0 +1,36 @@
+package com.main;
+import java.io.IOException;
+import java.io.StringWriter;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class SuperManOptionalTag extends SimpleTagSupport{
+	private String count;
+	StringWriter sw = new StringWriter();
+	
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+	
+	public void doTag() throws JspException, IOException {
+    	int c = 0;
+		JspWriter out = getJspContext().getOut();
+		
+    	if (getCount() != null) {
+    		c = Integer.parseInt(getCount());
+    		getJspBody().invoke(sw);
+	    } else {
+	    	getJspBody().invoke(sw);
+	    }
+    	// out.println("Hello Custom Tag!");
+    	for(int i = 0; i < c; i++) {
+    	   out.print(sw + "<br/>");
+    	}
+
+    }
+}	
+	
